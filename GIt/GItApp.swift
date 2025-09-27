41437368fb421e4f9d712256d0d70b451a1d682d
Andrei Rauh <rauhandrei69@gmail.com>
2025-09-27 19:41:31 +0300

    Initial Commit

---
+++ GIt/GItApp.swift
@@ -0,0 +1,17 @@
+//
+//  GItApp.swift
+//  GIt
+//
+//  Created by Andrei Rauh on 27.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct GItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
